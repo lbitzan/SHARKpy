718e100a32869d4011cbc6b6f8ecc6e56279f94c
lbitzan <ludwig.bitzan@gmx.de>
2025-06-22 01:46:44 +0200

    Cleaning 1

--- __init__.py
+++
@@ -1,8 +0,0 @@
-class KavInit:
-    def __init__(self):
-        self.variable1 = 0
-        self.variable2 = "default"
-        
-
-        # Add more variables and their default values as needed
-
